eedaf0b6beae2dfa4485a5f613062a16bfa3e6bc
Madeline Adee <m.adee@berkeley.edu>
2022-04-26 21:56:49 +0000

    added base structure for tests, what is the new egg file (???)

---
+++ ligotools/tests/tests_readligo.py
@@ -0,0 +1,12 @@
+
+from ligotools import readligo
+
+def test_read_hdf5():
+	
+    assert read_hdf5() == H-H1_LOSC_4_V2-1126259446-32.hdf5
+
+def test_toy_0():
+    assert toy(0) == 1
+
+def test_toy_1():
+    assert toy(1) == 2
