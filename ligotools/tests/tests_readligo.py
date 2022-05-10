adba2d57f67c5e58b97d97e1de774d10109c72cf
Madeline Adee <m.adee@berkeley.edu>
2022-05-10 04:29:58 +0000

    Added all tests

--- ligotools/tests/tests_readligo.py
+++ ligotools/tests/tests_readligo.py
@@ -1,14 +1,29 @@
 
 from ligotools import readligo
 
-def test_read_hdf5():
-name = "H-H1_LOSC_4_V2-1126259446-32.hdf5"
-strain_H1, time_H1, chan_dict_H1 = read_hdf5(name)
+# 4 different tests for the functions to load data /read hdf5 data
+# that check to make sure the length of the array is correct (for the two different data sets)
 
-    assert len(strain_H1) == 131072
+# test 1
+def test_read_hdf5_H():
+    name = "H-H1_LOSC_4_V2-1126259446-32.hdf5"
+	temp = read_hdf5(name)
+	assert len(temp) == 131072
 
-def test_toy_0():
-    assert toy(0) == 1
+# test 2
+def test_loaddata_H():
+    name = "H-H1_LOSC_4_V2-1126259446-32.hdf5"
+    temp = loaddata('data/'+fn_H1, 'H1')
+    assert len(temp) == 131072
 
-def test_toy_1():
-    assert toy(1) == 2
+# test 3
+def test_read_hdf5_L():
+    name = "L-L1_LOSC_4_V2-1126259446-32.hdf5"
+    temp = read_hdf5(name)
+    assert len(temp) == 131072
+
+# test 4
+def test_loaddata_L():
+    name = "L-L1_LOSC_4_V2-1126259446-32.hdf5"
+    temp = loaddata('data/'+fn_H1, 'H1')
+    assert len(temp) == 131072
