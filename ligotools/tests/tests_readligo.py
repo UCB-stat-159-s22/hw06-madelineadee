4d2b4c34d2807d7436d122910c600d48332fb06d
Madeline Adee <m.adee@berkeley.edu>
2022-05-10 04:12:54 +0000

    Added one test

--- ligotools/tests/tests_readligo.py
+++ ligotools/tests/tests_readligo.py
@@ -2,8 +2,10 @@
 from ligotools import readligo
 
 def test_read_hdf5():
-	
-    assert read_hdf5() == 
+name = "H-H1_LOSC_4_V2-1126259446-32.hdf5"
+strain_H1, time_H1, chan_dict_H1 = read_hdf5(name)
+
+    assert len(strain_H1) == 131072
 
 def test_toy_0():
     assert toy(0) == 1
